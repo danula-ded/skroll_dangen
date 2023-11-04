7a228c947eb1e0f7cb102673e9fd247d56a47849
danula-ded <117920938+danula-ded@users.noreply.github.com>
2023-11-04 17:08:38 +0300

    main
    main fails

---
+++ skroll-dangen/js/scripts.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+    direction: 'vertical',
+    speed: 2000,
+    mousewheel:{
+        enabled: true,
+        sensitivity: 2.4,
+    },
+    spaceBetween: 18,
+    parallax: true,
+    freeMode: true,
+})
